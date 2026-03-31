2959029de98c23822d365018fc81fd418bd7d730
nic-use-python <nicomantzouranis@gmail.com>
2026-03-31 15:23:12 +0200

    Add license and copyright information
    Added SPDX license and copyright notice to the file.

--- sara/autostart/autostart1.py
+++ sara/autostart/autostart1.py
@@ -1,4 +1,8 @@
-# Nutze die Namen, die du in local_vars übergeben hast
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2026 nic-use-python
+
+# diser code ist ein test für plugins
+# diser code ist ein beichspiel und nicht zum betrib notwendig
 print("Autostart läuft...")
 memory = tools.memory_print()
 ai.say("Das Gedächtnis ist: " + " ".join(memory))
